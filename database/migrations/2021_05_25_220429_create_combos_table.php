9ba16292e550aa1c8745513f8b1e82baea7ffaa7
vladimirmartinez-ef <vemf.vemf@hotmail.com>
2021-05-25 16:42:46 -0600

    arreglo completo

---
+++ database/migrations/2021_05_25_220429_create_combos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCombosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('combos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre', 100);
+            $table->string('descripcion');
+            $table->double('precio', 2, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('combos');
+    }
+}
---
+++ database/migrations/2021_05_25_220534_create_ventas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('cantidad');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('combo_id');
+            $table->foreign('combo_id')->references('id')->on('combos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
