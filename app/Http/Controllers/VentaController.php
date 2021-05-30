feb1cd9cd5a7cab1dcff8e6d03eadb49aa4f3cda
cmargueiz <c.margueiz@gmail.com>
2021-05-30 14:20:36 -0600

    Controladores de venta y combo

---
+++ app/Http/Controllers/VentaController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Venta;
+use App\Combo;
+use Illuminate\Http\Request;
+
+class VentaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            return Venta::where('user_id',auth()->id)->get();
+        }else{
+            return view('home');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $venta =  new Venta();
+        $venta->user_id = auth()->id();
+        $venta->combo_id = Combo::findOrFail($request->combo_id);
+        $venta->save();
+        return $nota;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $venta =  Venta::find($id);
+        $venta->user_id = auth()->id();
+        $venta->combo_id = $request->combo_id;
+        $venta->save();
+        return $combo
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $venta = Venta::find($id);
+        $venta->delete();
+    }
+}
